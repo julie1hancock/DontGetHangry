1fcf2b8af6ec09da73d2a61b0a4fd72ccb3900ba
Julie Hancock <julieh1023@gmail.com>
2020-04-03 13:16:44 -0600

    Add singleton for rest options

---
+++ app/src/main/java/hancock/julie/dontgethangry/Restaurant.kt
@@ -0,0 +1,13 @@
+package hancock.julie.dontgethangry
+
+data class Restaurant (
+    var name: String, //"Chick-fil-A"
+//    var image: String,
+    var type: String, //"Fast Food"
+    var price: String, //"$"
+    var address: String, //"484 W Bulldog Ln"
+    var milesAway: Double, //7.2
+    var hours: String, //10am-8pm
+    var website: String, //"www.chickfila.com"
+    var rating: Double
+)
