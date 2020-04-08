3a29a34ebf3cea9db72085efcff47c5a761ade73
Julie Hancock <julieh1023@gmail.com>
2020-04-08 01:38:57 -0600

    end done?

--- app/src/main/java/hancock/julie/dontgethangry/views/EndActivity.kt
+++ app/src/main/java/hancock/julie/dontgethangry/views/EndActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import hancock.julie.dontgethangry.R
@@ -21,8 +22,8 @@ import kotlinx.android.synthetic.main.rest_view.view.*
 class EndActivity : AppCompatActivity() {
 
     lateinit var presenter: EndPresenter
-    lateinit var linearLayoutManager: LinearLayoutManager
-
+//    lateinit var linearLayoutManager: LinearLayoutManager
+    lateinit var gridLayoutManager: GridLayoutManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_end)
@@ -55,27 +56,30 @@ class EndActivity : AppCompatActivity() {
     }
 
     private fun setupWinView() {
+        toolbarTitle.text = "Hanger Avoided!"
         winningLayout.visibility = View.VISIBLE
-        startOverBtnWin.visibility = View.VISIBLE
-        startOverBtnLose.visibility = View.GONE
+        startOverBottomLayoutWin.visibility = View.VISIBLE
+        startOverBottomLayoutLose.visibility = View.GONE
         losingLayout.visibility = View.GONE
 
-        linearLayoutManager = LinearLayoutManager(this)
-        winningRV.layoutManager = linearLayoutManager
+        gridLayoutManager = GridLayoutManager(this,3)
+//        linearLayoutManager = LinearLayoutManager(this)
+        winningRV.layoutManager = gridLayoutManager
 
         winningRV.adapter = EndAdapter(presenter.overlappingRests, this)
     }
 
     private fun setupLoseView() {
+        toolbarTitle.text = "Hanger Not Avoided!"
         winningLayout.visibility = View.GONE
-        startOverBtnWin.visibility = View.GONE
-        startOverBtnLose.visibility = View.VISIBLE
+        startOverBottomLayoutWin.visibility = View.GONE
+        startOverBottomLayoutLose.visibility = View.VISIBLE
         losingLayout.visibility = View.VISIBLE
 
-        linearLayoutManager = LinearLayoutManager(this)
-        theyPickedRV.layoutManager = linearLayoutManager
-        linearLayoutManager = LinearLayoutManager(this)
-        youPickedRV.layoutManager = linearLayoutManager
+        gridLayoutManager = GridLayoutManager(this,3)
+        theyPickedRV.layoutManager = gridLayoutManager
+        gridLayoutManager = GridLayoutManager(this,3)
+        youPickedRV.layoutManager = gridLayoutManager
 
         youPickedRV.adapter = EndAdapter(Singleton.youPicked, this)
         theyPickedRV.adapter = EndAdapter(Singleton.theyPicked, this)
@@ -94,15 +98,15 @@ class EndAdapter(private val rests: List<Restaurant>, val context: Context) :
 
     override fun onBindViewHolder(holder: EndHolder, position: Int) {
         holder.name.text = rests[position].name
-        holder.icon.setImageResource(R.drawable.ic_location_foreground)
-        holder.navigationRoot.setOnClickListener {
+        holder.icon.setImageResource(rests[position].image)
+        holder.root.setOnClickListener {
             Toast.makeText(context, rests[position].address, Toast.LENGTH_SHORT).show()
         }
     }
 
     class EndHolder(v: View) : RecyclerView.ViewHolder(v) {
-        val name = v.restLineRestName
-        val icon = v.restLineMarkerIcon
-        val navigationRoot = v.navigationLayout
+        val name = v.endRestName
+        val icon = v.endRestIcon
+        val root = v.endRoot
     }
 }
